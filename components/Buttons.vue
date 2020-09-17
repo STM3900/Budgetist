77f128fe97b43a80ded17874d1ab04ffe699f930
Théo <theo_38180@live.fr>
2020-09-17 14:49:01 +0200

    Ajout d'un message de confirmation lorsque l'on veut supprimer notre profil
    Parce qu'un missclick est si vite arrivé

--- components/Buttons.vue
+++ components/Buttons.vue
@@ -5,7 +5,20 @@
           <nuxt-link to="budget"><v-btn color="info" block outlined><v-icon left>arrow_right_alt</v-icon>Aller au budget</v-btn></nuxt-link> 
         </v-col>
         <v-col sm="5">
-          <v-btn color="warning" outlined block v-on:click="$emit('reset');"><v-icon left>delete</v-icon>Réinitialiser votre profil</v-btn>
+            <v-dialog v-model="dialog" max-width="600px">
+                <template v-slot:activator="{ on, attrs }">
+                <v-btn color="warning" outlined block v-bind="attrs" v-on="on"><v-icon left>delete</v-icon>Réinitialiser votre profil</v-btn>
+                </template>
+                <v-card>
+                <v-card-title class="headline error white--text"><v-icon left color="white">warning</v-icon>Êtes-vous sur ?</v-card-title>
+                <v-card-text class="pt-5 pb-3">Cela supprimera définitivement toutes vos dépenses. Continuer ?</v-card-text>
+                <v-card-actions class="pt-0">
+                    <v-btn color="error" text @click="dialog = false, $emit('reset')">Oui</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn color="info" text @click="dialog = false">Noooon</v-btn>
+                </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-col>
       </v-col>
     </v-container>
@@ -13,6 +26,11 @@
 
 <script>
 export default {
-    name: 'Buttons'
+    name: 'Buttons',
+    data: function(){
+        return{
+            dialog: false
+        }
+    }
 }
 </script>
